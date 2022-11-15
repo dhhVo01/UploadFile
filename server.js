c0b3d3f07d169fbc3dc468d12f242cebf97aba68
Dangvhh <vohuynhhaidang24032001@gmail.com>
2022-11-15 13:40:50 +0700

    fix server

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 import express from "express";
 import { spawn } from "child_process";
+import XLSX from "xlsx";
+import formidable from "formidable";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", function(req, res){
-    let process = spawn("python", ["./controllers/app.py","a","b"]);
-    process.stdout.on("data", function(data){
-        res.send(data.toString());
-    })
+
+app.post("/api/upload", function(req, res){
+    const options = {
+        keepExtensions: true
+    }
+    const form = new formidable.IncomingForm(options);
+    form.parse(req, (err, fields, files) => {
+    /* grab the first file */
+        const f = Object.entries(files)[0][1];
+        const path = f.filepath;
+        const workbook = XLSX.readFile(path);
+
+    /* DO SOMETHING WITH workbook HERE */
+    res.send(path);
+    // var process = spawn("py", ["./controllers/app.py", path]);
+    // process.stdout.on("data", function(data){
+    //     console.log(data.toString());
+    // })
+    // process.stderr.on('data', (data) => {
+    //     console.error(data.toString());
+    //   });
+  });
 })
 
 
